4c3e5cb0317cfa86bd17bbc305feee076ca2a3b3
Marcos Sombra <markos.vs@live.com>
2017-04-22 13:45:47 -0300

    submit teste

---
+++ Questões 03 e 04/Personagem.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Personagem{
+	//atributos
+	private int peso;
+	private int pesoAdicional;
+	private int ataque;
+	private int defesa;
+	private int hp;
+	private ArrayList<Item> itens;
+}
