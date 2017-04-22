0cf496df8eaaca8be4ef744b057a579cfa138ead
Marcos Sombra <markos.vs@live.com>
2017-04-22 17:46:10 -0300

    inserir getters

--- Questões 03 e 04/Personagem.java
+++ Questões 03 e 04/Personagem.java
@@ -8,4 +8,29 @@ public class Personagem{
 	private int defesa;
 	private int hp;
 	private ArrayList<Item> itens;
+
+	//getters
+	public int getPeso(){
+		return this.peso;
+	}
+
+	public int getPesoAdicional(){
+		return this.pesoAdicional;
+	}
+
+	public int getAtaque(){
+		return this.ataque;
+	}
+
+	public int getDefesa(){
+		return this.defesa;
+	}
+
+	public int getHp(){
+		return this.hp;
+	}
+
+	public ArrayList<Item> getItens(){
+		return this.itens;
+	}
 }
